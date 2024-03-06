690020e7b594b88e5cfa85120060f7f0b2efb784
Matt Heslington <matt.heslington@icloud.com>
2024-03-06 23:23:55 +0800

    like button: create wishlist, fetch wishlists

--- src/lib/components/card/like.svelte
+++ src/lib/components/card/like.svelte
@@ -1,27 +1,125 @@
 <script>
+	import * as Collapsible from '$lib/components/ui/collapsible'
+	import * as Dialog from '$lib/components/ui/dialog'
+	import * as Select from '$lib/components/ui/select'
+	import { wishlists } from './wishlistStore.js'
+	import { fetchData } from '$lib/utils/fetchData.js'
+	import { onMount } from 'svelte'
+	import { Button } from '$lib/components/ui/button'
+	import { Input } from '$lib/components/ui/input'
 	import { Heart } from 'lucide-svelte'
 	import { toast } from 'svelte-sonner'
+	import { user } from '$lib/globalStore.js'
 
-	const loggedIn = true
+	let authUser
+	user.subscribe((value) => {
+		authUser = value
+	})
+
 	let liked = false
+	let loading = false
+	let wishlistName = ''
+	let uiWishlists = [
+		{
+			id: 0,
+			wishlistName: 'My favourite places'
+		}
+	]
 
-	function handleLike() {
+	$: {
+		console.log('wishlistName', wishlistName)
+	}
+
+	$: {
+		console.log('uiWishlists', uiWishlists)
+	}
+
+	async function like(selected) {
+		wishlistName = selected
 		liked = true
+		// await fetchData('/api/likes/add', 'POST', {
+		// 	listingId: listingId,
+		// 	ownerId: ownerId,
+		// 	wishlistId: wishlistId
+		// })
 		toast('Villa Liked')
 	}
 
-	function handleUnLike() {
+	function unlike() {
 		liked = false
 		toast('Like Removed')
 	}
+
+	async function getWishlists() {
+		const userWishlists = await fetchData(
+			`https://api.ultimatevillas.workers.dev/api/wishlists/get-user-wishlists?userId=${authUser.userId}`,
+			'GET'
+		)
+		//console.log('userWishlists', userWishlists)
+		// wishlists.set(userWishlists)
+		if (userWishlists.length > 0) {
+			uiWishlists = userWishlists
+		}
+	}
+
+	async function createNewWishlist() {
+		liked = true
+		const slug = wishlistName
+			.trim()
+			.toLowerCase()
+			.replace(/[ ,.]+/g, '-')
+		await fetchData('https://api.ultimatevillas.workers.dev/api/wishlists/add-new-wishlist', 'POST', {
+			userId: authUser.userId,
+			username: authUser.username,
+			wishlistName: wishlistName,
+			wishlistSlug: slug
+		})
+		toast('Villa Liked')
+		// close dialog
+		open.set(false)
+	}
+
+	onMount(() => {
+		getWishlists()
+	})
 </script>
 
 {#if liked}
-	<button on:click={handleUnLike}>
+	<button on:click={unlike}>
 		<Heart color="black" fill="#FFF" strokeWidth={1} />
 	</button>
 {:else}
-	<button on:click={handleLike}>
-		<Heart color="white" fill="#000" strokeWidth={1} />
-	</button>
+	<Dialog.Root>
+		<Dialog.Trigger>
+			<Heart color="white" fill="#000" strokeWidth={1} />
+		</Dialog.Trigger>
+		<Dialog.Content class="max-h-[85vh] w-[90vw] max-w-[350px] p-6">
+			<Dialog.Header>
+				<Dialog.Title>Add to your favourite places</Dialog.Title>
+				<Dialog.Description>Save this villa to one of your lists, or create a new list.</Dialog.Description>
+			</Dialog.Header>
+			<Select.Root
+				onSelectedChange={(v) => {
+					v && like(v.value)
+				}}
+				class="w-full">
+				<Select.Trigger class="w-full border border-border">
+					<Select.Value placeholder="Select list" />
+				</Select.Trigger>
+				<Select.Content>
+					{#each uiWishlists as x}
+						<Select.Item value={x.wishlistName}>{x.wishlistName}</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+
+			<Collapsible.Root class="w-full">
+				<Collapsible.Trigger class="w-full"><Button variant="outline" class="w-full">Add a new list</Button></Collapsible.Trigger>
+				<Collapsible.Content class="mt-3 grid grid-cols-12 gap-3">
+					<Input bind:value={wishlistName} type="text" placeholder="e.g. Birthday Villas" class="col-span-8" />
+					<Button on:click={createNewWishlist} variant="outline" class="col-span-4">Save</Button>
+				</Collapsible.Content>
+			</Collapsible.Root>
+		</Dialog.Content>
+	</Dialog.Root>
 {/if}
--- src/lib/components/header/account-button/account-button.svelte
+++ src/lib/components/header/account-button/account-button.svelte
@@ -11,24 +11,28 @@
 	})
 </script>
 
-<DropdownMenu.Root>
-	<DropdownMenu.Trigger>
-		<Button variant="outline" class="mt-[6px] h-9 w-9">
-			<Avatar.Root class="h-9 w-9 rounded-md">
-				<Avatar.Image src={authUser.avatar} alt={authUser.username} class="object-cover object-center" />
-				<Avatar.Fallback>{authUser.fallback}</Avatar.Fallback>
-			</Avatar.Root>
-		</Button>
-	</DropdownMenu.Trigger>
-	<DropdownMenu.Content class="grid h-[360px] w-[768px] grid-cols-3 grid-rows-4 justify-between gap-2 rounded-md p-2 ">
-		<DropdownMenu.Item class="col-span-1 row-span-4 rounded-md bg-muted">Video</DropdownMenu.Item>
-		{#each buttons as button}
-			<DropdownMenu.Item
-				href={button.url}
-				class="col-span-1 row-span-1 flex h-full w-full cursor-pointer flex-col items-start justify-start space-y-[2px] rounded-md">
-				<h4 class="font-bold">{button.title}</h4>
-				<p class="line-clamp-2 text-left text-[14px] leading-snug text-muted-foreground">{button.body}</p>
-			</DropdownMenu.Item>
-		{/each}
-	</DropdownMenu.Content>
-</DropdownMenu.Root>
+{#if authUser.loggedIn}
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger>
+			<Button variant="outline" class="mt-[6px] h-9 w-9">
+				<Avatar.Root class="h-9 w-9 rounded-md">
+					<Avatar.Image src={authUser.avatar} alt={authUser.username} class="object-cover object-center" />
+					<Avatar.Fallback>{authUser.fallback}</Avatar.Fallback>
+				</Avatar.Root>
+			</Button>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content class="grid h-[360px] w-[768px] grid-cols-3 grid-rows-4 justify-between gap-2 rounded-md p-2 ">
+			<DropdownMenu.Item class="col-span-1 row-span-4 rounded-md bg-muted">Video</DropdownMenu.Item>
+			{#each buttons as button}
+				<DropdownMenu.Item
+					href={button.url}
+					class="col-span-1 row-span-1 flex h-full w-full cursor-pointer flex-col items-start justify-start space-y-[2px] rounded-md">
+					<h4 class="font-bold">{button.title}</h4>
+					<p class="line-clamp-2 text-left text-[14px] leading-snug text-muted-foreground">{button.body}</p>
+				</DropdownMenu.Item>
+			{/each}
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+{:else}
+	<Button variant="outline">Login</Button>
+{/if}
