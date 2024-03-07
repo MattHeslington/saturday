3c025eded956e47ac0025f391c493b4394ba2892
Matt Heslington <matt.heslington@icloud.com>
2024-03-07 22:28:53 +0800

    new add your place design

---
+++ src/lib/components/ui/sheet/sheet-overlay.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Dialog as SheetPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils";
+	import { fade } from "svelte/transition";
+	let className = undefined;
+	export { className as class };
+	export let transition = fade;
+	export let transitionConfig = {
+		duration: 150,
+	};
+</script>
+
+<SheetPrimitive.Overlay
+	{transition}
+	{transitionConfig}
+	class={cn("fixed inset-0 z-50 bg-background/80 backdrop-blur-sm", className)}
+	{...$$restProps}
+/>
---
+++ src/routes/(app)/add-your-place/amenitiesSpecial.js
@@ -0,0 +1,62 @@
+export const amenitiesSpecial = [
+	{
+		label: 'breakfasts',
+		value: 'breakfasts',
+		icon: ''
+	},
+	{
+		label: 'housekeeping',
+		value: 'housekeeping',
+		icon: ''
+	},
+	{
+		label: 'chef',
+		value: 'chef',
+		icon: ''
+	},
+	{
+		label: 'butler',
+		value: 'butler',
+		icon: ''
+	},
+	{
+		label: 'property manager',
+		value: 'property-manager',
+		icon: ''
+	},
+	{
+		label: 'security staff',
+		value: 'security-staff',
+		icon: ''
+	},
+	{
+		label: 'car & driver',
+		value: 'car-and-driver',
+		icon: ''
+	},
+	{
+		label: 'gym equipment',
+		value: 'gym-equipment',
+		icon: ''
+	},
+	{
+		label: 'pool table',
+		value: 'pool-table',
+		icon: ''
+	},
+	{
+		label: 'barbecue',
+		value: 'barbecue',
+		icon: ''
+	},
+	{
+		label: 'jacuzzi',
+		value: 'jacuzzi',
+		icon: ''
+	},
+	{
+		label: 'welcome drinks',
+		value: 'welcome-drinks',
+		icon: ''
+	}
+]
